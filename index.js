1b75c57c880457710b184a79fc052e310e11f9f8
VayNBlayN <88253969+VayNBlayN@users.noreply.github.com>
2025-07-26 19:54:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
 // Socket.IO connection
